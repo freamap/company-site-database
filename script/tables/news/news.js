614c9e230721d28f899270bf95e869b1eeda4d9d
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-19 20:26:57 +0900

    スループットのユニット数を修正

--- script/tables/news/news.js
+++ script/tables/news/news.js
@@ -7,7 +7,7 @@ module.exports = {
       { AttributeName: "news_id", AttributeType: "N" }
   ],
   ProvisionedThroughput: {       
-      ReadCapacityUnits: 4, // 1項目500文字(title:100文字 description:300文字　その他:100文字) * 10項目 * 3回/秒 = 15KB
+      ReadCapacityUnits: 5, // 1項目550文字(title:150文字 description:300文字　その他:100文字) * 10項目 * 3回/秒 = 16.5KB
       WriteCapacityUnits: 1
   }
 }
--- script/tables/news/news_detail.js
+++ script/tables/news/news_detail.js
@@ -9,7 +9,7 @@ module.exports = {
       { AttributeName: "page", AttributeType: "N" }
   ],
   ProvisionedThroughput: {       
-      ReadCapacityUnits: 4, //  1項目3100文字(コンテンツ:3000文字 その他:100文字) * 4回 = 12.4KB
-      WriteCapacityUnits: 1 // 1項目3100文字(コンテンツ:3000文字 その他:100文字) * 1回 = 3.1KB
+      ReadCapacityUnits: 5, //  1項目6250文字(コンテンツ:6000文字 タイトル:150文字 その他:100文字) * 4回/秒 = 25KB
+      WriteCapacityUnits: 2 // 1項目6250文字(コンテンツ:6000文字 タイトル:150文字 その他:100文字) * 1回/秒 = 6.25KB
   }
 }
