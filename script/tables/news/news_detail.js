607923ce613eb4655e8ed3e3f777e58fdc526b96
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-20 11:39:01 +0900

    news_detailにもdescriptionを作成

--- script/tables/news/news_detail.js
+++ script/tables/news/news_detail.js
@@ -8,8 +8,8 @@ module.exports = {
     { AttributeName: "news_id", AttributeType: "N" },
     { AttributeName: "page", AttributeType: "N" }
   ],
-  ProvisionedThroughput: {       
-    ReadCapacityUnits: 5, //  1項目6250文字(コンテンツ:6000文字 タイトル:150文字 その他:100文字) * 4回/秒 = 25KB
-    WriteCapacityUnits: 2 // 1項目6250文字(コンテンツ:6000文字 タイトル:150文字 その他:100文字) * 1回/秒 = 6.25KB
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 7, //  1項目6250文字(コンテンツ:6000文字 タイトル:150文字 description:300文字 その他:100文字) * 4回/秒 = 26.5KB
+    WriteCapacityUnits: 2 // 1項目6250文字(コンテンツ:6000文字 タイトル:150文字 description:300文字 その他:100文字) * 1回/秒 = 6.55KB
   }
 }
