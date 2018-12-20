00ba860e220279b395632ab43c7f471052113857
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-21 01:50:11 +0900

    newsテーブル変更

--- script/tables/news/news.js
+++ script/tables/news/news.js
@@ -1,15 +1,37 @@
 module.exports = {
   TableName : "news",
   KeySchema: [
-    { AttributeName: "create", KeyType: "HASH"},
-    { AttributeName: "news_id", KeyType: "RANGE"}
+    { AttributeName: "news_id", KeyType: "HASH"}
   ],
   AttributeDefinitions: [
+    { AttributeName: "news_id", AttributeType: "N" },
+    { AttributeName: "pickup", AttributeType: "N" },
     { AttributeName: "create", AttributeType: "S" },
-    { AttributeName: "news_id", AttributeType: "N" }
   ],
-  ProvisionedThroughput: {       
-    ReadCapacityUnits: 5, // 1項目550文字(title:150文字 description:300文字　その他:100文字) * 10項目 * 3回/秒 = 16.5KB
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 3, // 1項目550文字(title:150文字 description:300文字　その他:100文字) * 10項目 * 3回/秒 = 16.5KB
     WriteCapacityUnits: 1
-  }
+  },
+  GlobalSecondaryIndexes: [
+    {
+      IndexName: "PickupSortCreateIndex",
+      KeySchema: [
+        {
+          AttributeName: "pickup",
+          KeyType: "HASH"
+        },
+        {
+          AttributeName: "create",
+          KeyType: "RANGE"
+        }
+      ],
+      Projection: {
+        ProjectionType: "ALL"
+      },
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 3, // 1項目550文字(title:150文字 description:300文字　その他:100文字) * 10項目 * 3回/秒 = 16.5KB
+        WriteCapacityUnits: 1
+      }
+    }
+  ]
 }
